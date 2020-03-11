d7eb9ea9431fc7df0eeda7234d5e90ccd7e3a861
napat <napatkrup@hotmail.com>
2020-03-11 15:32:22 +0700

    :zap: KodVote V.5 AddPassword

--- KodVote/management/views.py
+++ KodVote/management/views.py
@@ -1,39 +1,55 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 from polls.models import Poll
 
 def my_login(request):
     context = {}
+
+    # Get detail in form
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
+
+        # Success
         if user:
             login(request, user)
+
+            # Redirect
             next_url = request.POST.get('next_url')
-            if next_url:
+            if next_url != '':
                 return redirect(next_url)
             else:
                 return redirect('home')
+
+        # Fail
         else:
             context['username'] = username
             context['password'] = password
             context['error'] = 'Wrong username or password!'
+
+    # Get url path
     next_url = request.GET.get('next')
     if next_url:
         context['next_url'] = next_url
+
     return render(request, template_name='management/login.html', context=context)
+
 
 def register(request):
     context = {}
+
+    # Get detail in form
     if (request.method == 'POST'):
         first_name = request.POST.get('fname')
         last_name = request.POST.get('lname')
         email = request.POST.get('email')
         username = request.POST.get('username')
         password = request.POST.get('password')
+
+        # Check already used
         if (User.objects.filter(username=username).exists()):
             context['error'] = 'Username is already used!'
             context['fname'] = first_name
@@ -42,6 +58,8 @@ def register(request):
             context['username'] = username
             context['password'] = password
             return render(request, 'management/register.html', context=context)
+
+        # Add user to DB
         user = User.objects.create_user(
             username=username,
             password=password,
@@ -49,43 +67,60 @@ def register(request):
             last_name=last_name,
             email=email
         )
+
         login(request, user)
         return redirect('home')
+
     return render(request, template_name='management/register.html', context=context)
 
-@login_required
+
+@login_required(login_url='login')
 def my_logout(request):
     logout(request)
     return redirect('login')
 
 
-@login_required
+@login_required(login_url='login')
 def change_password(request):
     user = request.user
     context = {}
     context['username'] = user.username
+
+    # Get detail in form
     if request.method == 'POST':
         password1 = request.POST.get('npass1')
         password2 = request.POST.get('npass2')
+
+        # Check password matching
         if password1 != password2:
             context['error'] = 'Password not match!'
             return render(request, template_name='management/change_password.html', context=context)
+
+        # Set password to DB
         user.set_password(password1)
         user.save()
         logout(request)
         return redirect('login')
+
     return render(request, template_name='management/change_password.html', context=context)
 
-@login_required
+
+@login_required(login_url='login')
 def my_poll(request):
     user = request.user
-    polls = Poll.objects.filter(create_by=request.user).order_by('start_date')
+
+    # Get all my poll
+    start_polls = Poll.objects.filter(create_by=request.user).order_by('-start_date')
+    end_polls = Poll.objects.filter(create_by=request.user).order_by('-end_date')
     available, closed = [], []
-    for poll in polls:
+    for poll in start_polls:
         if poll.is_available():
             available.append(poll)
-        else:
+    for poll in end_polls:
+        if not poll.is_available():
             closed.append(poll)
+
+    # Send Context
     context = {
         'fname' : user.first_name,
         'lname' : user.last_name,
@@ -94,4 +129,5 @@ def my_poll(request):
         'available' : available,
         'closed' : closed
     }
+
     return render(request, 'management/my_poll.html', context=context)
--- KodVote/polls/models.py
+++ KodVote/polls/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 from django.utils import timezone
 
 class Poll(models.Model):
-    subject = models.CharField(max_length=100, blank=False)
-    detail = models.TextField(blank=False)
+    subject = models.CharField(max_length=255, blank=False, null=False)
+    detail = models.TextField(blank=False, null=False)
     picture = models.ImageField(default="polls/default.png", upload_to='polls/')
-    start_date = models.DateTimeField(default=timezone.now)
-    end_date = models.DateTimeField()
-    password = models.CharField(max_length=100, blank=False)
+    start_date = models.DateTimeField(default=datetime.now, blank=False, null=False)
+    end_date = models.DateTimeField(blank=False, null=False)
+    password = models.CharField(max_length=255, blank=False, null=False)
     create_by = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
     def is_available(self):
-        if timezone.now() >= self.end_date:
+        if (timezone.now() >= self.end_date) or (timezone.now() < self.start_date):
             self.is_active = False
+            self.save()
+        else:
+            self.is_active = True
             self.save()
         return self.is_active
 
 class Poll_Choice(models.Model):
-    subject = models.CharField(max_length=100, blank=False)
+    subject = models.CharField(max_length=255, blank=False, null=False)
     image = models.ImageField(default='choices/default.png', upload_to='choices/')
     poll_id = models.ForeignKey(Poll, on_delete=models.CASCADE)
 
